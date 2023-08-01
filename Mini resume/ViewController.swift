73114f62f3a3831518dc56bddab9a03a9c9fce5d
Elenka <kholodilinaes@tech.bcs.ru>
2023-08-01 00:56:37 -0700

    Initial Commit

---
+++ Mini resume/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mini resume
+//
+//  Created by Oksenoyt on 01.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
